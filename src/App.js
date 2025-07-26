4d1fe6a52d58c1ef7c2c113c7d352091075814ae
Nagadurga <mndurga22004@gmail.com>
2025-07-26 15:40:02 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let name = "durga";
+  return (
+    <>
+    <h1>Naga Durga</h1>
+    <h1>{name}</h1>
+    </>
+  );
+}
+
+export default App;
